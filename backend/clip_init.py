7b1eda825b3e6b43c8da82c19a96ac63b9cc1f3d
Mike Xydas <mikexydas@gmail.com>
2021-08-29 01:11:47 +0300

    Delete clip_init.py

--- backend/clip_init.py
+++
@@ -1,8 +0,0 @@
-"""
-This file is used to download the CLIP model when building the Dockerfile.
-"""
-import clip
-import torch
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model, preprocess = clip.load("ViT-B/32", device=device)
